ca8d6acbc2f7dfb10a5da989885a9f3ec2ae143f
Matthew Carson <matthew.carson@pm.me>
2022-04-14 01:35:25 -0400

    Show note when it is clicked on

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
@@ -15,12 +16,20 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
   const { title, text } = req.body
+  console.log(title, text)
   fs.readFile(dbPath, { encoding: 'utf-8' }, (err, data) => {
     if (!err) {
-      const notes = JSON.parse(data)
-      notes.push({ title, text })
-      fs.writeFile(dbPath, JSON.stringify(notes), err => console.error(err))
-      res.redirect('/notes')
+      if (!title.trim().length || !text.trim().length) {
+        return res.status(400, '/notes').json({
+          status: 400,
+          message: 'Each note requires a title and text content'
+        })
+      } else {
+        const notes = JSON.parse(data)
+        notes.push({ title, text })
+        fs.writeFile(dbPath, JSON.stringify(notes), err => console.error(err))
+        res.redirect('/notes')
+      }
     } else console.error(err)
   })
 })
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,41 @@
 import $ from 'jquery'
 
+let notes
+
+const renderNotes = () => {
+  notes.forEach(note => {
+    const noteItem = $('<li>').text(note.title).addClass('flex justify-between py-3 px-3 text-xl odd:bg-slate-200 hover:cursor-pointer')
+    const svg = $(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>`)
+    svg.addClass('w-6 h-6 fill-red-500')
+    noteItem.click(() => {
+      $('form').hide()
+      $('#note').show()
+      $('#note-title').text(note.title)
+      $('#note-text').text(note.text)
+    })
+    noteItem.append(svg)
+    $('#saved-notes ul').append(noteItem)
+  });
+}
+
 $.when($.ready)
   .then(() => {
-    $.ajax('/api/notes')
+    $.ajax('/api/notes', {
+      error: err => console.log(err)
+    })
       .done(data => {
-        data.forEach(note => {
-          const noteItem = $('<li>').text(note.title).addClass('flex justify-between py-3 px-3 text-xl odd:bg-slate-200')
-          const svg = $(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>`)
-          svg.addClass('w-6 h-6 fill-red-500')
-          noteItem.append(svg)
-          $('#saved-notes ul').append(noteItem)
-        });
+        notes = data
+        renderNotes()
       })
   })
 
-$('#save-note').click(() => $('#new-note-form').submit())
+$('#save-note').click((e) => { 
+  $('#new-note-form').submit()
+  notes.push({
+    title: $('input[name="title"]').val(),
+    text: $('textarea[name="text"]').val()
+  })
+  renderNotes()
+  e.preventDefault()
+})
 $('#new-note').click(e => console.log('Add a note...'))
